2336078dfdbc37c8805d9a08b98e3a86897cdd70
Ghost <panov-eo@yandex.ru>
2016-08-23 13:38:29 +0300

    plugin created

---
+++ spec/insert-time-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import InsertTimeView from '../lib/insert-time-view';
+
+describe('InsertTimeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
